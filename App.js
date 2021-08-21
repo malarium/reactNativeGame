c85cdcc74b704a5fdc14b983b6d64649f0d1c674
Marcin <marcingorecki1981@gmail.com>
2021-08-21 12:00:04 +0200

    components separation

--- App.js
+++ App.js
@@ -8,75 +8,18 @@ import {
   BackHandler,
   useWindowDimensions,
 } from "react-native";
-
-const initPlay = () => {
-  console.log(`Gramy!`);
-};
-
-const initExit = () => {
-  BackHandler.exitApp();
-};
+import { MainMenu } from "./modules/MainMenu";
 
 const App = () => {
-  const win = useWindowDimensions();
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require(`./assets/background_letters.jpg`)}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <View style={styles.mainNavigation}>
-          <Pressable
-            style={styles.mainNavigation_btn}
-            onPress={initPlay}
-            left={(win.width - 200) / 2}
-          >
-            <Text style={styles.text}>Graj</Text>
-          </Pressable>
-          <Pressable
-            style={styles.mainNavigation_btn}
-            onPress={initExit}
-            left={(win.width - 200) / 2}
-          >
-            <Text style={styles.text}>Wyjdź</Text>
-          </Pressable>
-        </View>
-      </ImageBackground>
-    </View>
+  const [page, setPage] = React.useState(0);
+
+  return page === 0 ? (
+    <MainMenu setPage={setPage} />
+  ) : page === 1 ? (
+    <Text>TEST</Text>
+  ) : (
+    <Text>TEST2</Text>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  mainNavigation: {
-    display: "flex",
-    width: 100,
-  },
-  mainNavigation_btn: {
-    width: 200,
-    height: 200,
-    backgroundColor: "#f2f2f2",
-    marginTop: 20,
-    borderRadius: 100,
-    borderWidth: 20,
-    borderColor: "tomato",
-    justifyContent: "center", //Centered horizontally
-    alignItems: "center", //Centered vertically
-  },
-  text: {
-    textAlign: "center",
-    textAlignVertical: "center",
-    color: "black",
-    fontSize: 26,
-    fontWeight: "bold",
-    fontFamily: "monospace",
-  },
-});
 export default App;
---
+++ modules/MainMenu.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  BackHandler,
+  useWindowDimensions,
+  AppLoading,
+} from "react-native";
+import { useFonts } from "@expo-google-fonts/inter";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  mainNavigation: {
+    display: "flex",
+    width: 100,
+  },
+  mainNavigation_btn: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#f2f2f2",
+    marginTop: 20,
+    borderRadius: 100,
+    borderWidth: 20,
+    borderColor: "tomato",
+    justifyContent: "center", //Centered horizontally
+    alignItems: "center", //Centered vertically
+  },
+  text: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: "black",
+    fontSize: 26,
+    fontWeight: "bold",
+    fontFamily: "monospace",
+  },
+});
+
+export function MainMenu(props) {
+  const win = useWindowDimensions();
+
+  const initExit = () => {
+    BackHandler.exitApp();
+  };
+  const initPlay = () => {
+    console.log(`Gramy!`);
+    props.setPage(1);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require(`../assets/background_letters.jpg`)}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.mainNavigation}>
+          <Pressable
+            style={styles.mainNavigation_btn}
+            onPress={initPlay}
+            left={(win.width - 200) / 2}
+          >
+            <Text style={styles.text}>GRAJ</Text>
+          </Pressable>
+          <Pressable
+            style={styles.mainNavigation_btn}
+            onPress={initExit}
+            left={(win.width - 200) / 2}
+          >
+            <Text style={styles.text}>WYJDŹ</Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
